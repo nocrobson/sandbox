faf0363ceb0fd0f022d855fed502d09c0ecd3950
nocrobson <noc.robson@gmail.com>
2018-02-12 19:46:13 -0200

    Initial commit

---
+++ vue/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <pagina-login
+      v-if="!usuarioAutenticado"
+      @autenticateUser="releaseMainScreenApp">
+    </pagina-login>
+    <div v-else>
+      <nav>
+        <div class="nav-wrapper teal">
+          <a href="#" class="brand-logo" style="margin-left:15px; margin-top:5px;"><img src="./assets/logo.png" alt=""></a>
+          <ul id="nav-mobile" class="right hide-on-med-and-down">
+            <li><a href="#">Solicitar Marmita</a></li>
+            <li><a href="#">Gerenciar Marmitas</a></li>
+            <li><a href="#"><i class="material-icons">perm_identity</i></a></li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  </div>
+</template>
+  
+<script>
+import PaginaLogin from './shared/components/PaginaLogin.vue';
+
+export default {
+  name: 'app',
+  components: {
+    PaginaLogin
+  },
+  data () {
+    return {
+      usuarioAutenticado:false
+    }
+  },
+  methods:{
+    releaseMainScreenApp(){
+      this.usuarioAutenticado = true;
+    }
+  }
+}
+</script>
+
---
+++ vue/src/shared/components/PaginaLogin.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="container center-align" style="margin-top:5%">
+        <div class="row">
+            <div class="col s6 offset-s3">
+                <div class="card-panel ">
+                <div class="row">
+                    <form class="col s12">
+                        <div class="row">
+                            <div class="input-field col s12">
+                            <input id="login" type="text" class="validate" v-model="logInData.login" @keyup.enter="login">
+                            <label for="login">Login</label>
+                            </div>                    
+                        </div>
+                        <div class="row">
+                            <div class="input-field col s12">
+                            <input id="password" type="password" class="validate" v-model="logInData.password" @keyup.enter="login">
+                            <label for="password">Senha</label>
+                            </div>
+                        </div>    
+                        <div class="row">
+                            <form action="#">
+                                <p>
+                                <input type="checkbox" id="rememberme" true-value="Y" false-value="N" v-model="logInData.keepMeLoged"/>
+                                <label for="rememberme">Mantenha-me logado</label>
+                                </p>
+                            </form>
+                        </div>        
+                        <div class="row right">
+                            <a class="waves-effect waves-light btn"@click.prevent="login">Log In</a>
+                        </div>
+                    </form>
+                </div>  
+                </div>
+            </div>
+        </div>  
+    </div>
+</template>
+
+<script>
+export default {
+    name:'pagina-login',
+    data(){
+        return {
+            logInData:{
+                login:'',
+                password:'',
+                keepMeLoged:'N'
+            }
+        }
+    },
+    methods:{
+        login() {            
+            this.$emit('autenticateUser');
+        }
+    }
+
+}
+</script>
